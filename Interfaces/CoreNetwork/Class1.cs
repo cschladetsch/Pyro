6d5d6f058c38aee58fbef61b9c348add8efb7706
Christian Schladetsch <christian.schladetsch@gmail.com>
2018-12-17 23:47:32 +1100

    Add more tests

--- Interfaces/CoreNetwork/Class1.cs
+++ Interfaces/CoreNetwork/Class1.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using Diver.Executor;
 
 namespace Diver.Core.Network
@@ -8,8 +9,21 @@ namespace Diver.Core.Network
     /// </summary>
     public interface IPeer
     {
+        string Listen(int port);
+
         string Connect(string ipAddress, int port);
+        string Handshake(Guid node, int port);
+
         bool SendText(string text);
+        bool ReceiveText(IResponseBase response);
+
+        void Ping(DateTime sent);
+        DateTime Pong();
+
         List<object> Execute(IRef<Continuation> cont);
+    }
+
+    public interface IResponseBase
+    {
     }
 }
