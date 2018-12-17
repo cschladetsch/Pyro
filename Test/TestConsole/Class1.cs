ab1310cd436153dfc02d32784c4b10855f267f66
Christian Schladetsch <christian.schladetsch@gmail.com>
2018-12-17 21:53:07 +1100

    Fixed unit tests to all use NUnit; fixed .Net framewortk references

--- Test/TestConsole/Class1.cs
+++ Test/TestConsole/Class1.cs
@@ -4,12 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Console
+namespace TestConsole
 {
-    class Program
+    public class Class1
     {
-        static void Main(string[] args)
-        {
-        }
     }
 }
