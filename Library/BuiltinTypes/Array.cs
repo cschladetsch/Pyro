254aacb953b0a681dea5428bea1efd103fe61f20
Christian Schladetsch <christian.schladetsch@gmail.com>
2018-12-19 00:22:01 +1100

    Update flow

--- Library/BuiltinTypes/Array.cs
+++ Library/BuiltinTypes/Array.cs
@@ -6,6 +6,8 @@ namespace BuiltinTypes
     {
         public List<object> Objects => _objects;
         public int? Count => _objects?.Count;
+#pragma warning disable CS0649 // Field 'Array._objects' is never assigned to, and will always have its default value null
         private List<object> _objects;
+#pragma warning restore CS0649 // Field 'Array._objects' is never assigned to, and will always have its default value null
     }
 }
