2b17b9bf4cd4e4c0e63162f97c1ab565e38516a6
Christian Schladetsch <christian.schladetsch@gmail.com>
2018-12-20 19:40:21 +1100

    Add parameterised tests for Executor

--- Test/TestExecutor/TestExecutor.cs
+++ Test/TestExecutor/TestExecutor.cs
@@ -7,21 +7,32 @@ namespace Diver.Tests
     [TestFixture]
     public class TestExecutor : TestCommon
     {
-        [Test]
-        public void TestPrimitives()
+        public void TestPrimitives(object a, object b, object sum)
         {
             var code = _reg.Add(new List<object>());
             var coro = _reg.Add(new Continuation(code.Value));
 
-            code.Value.Add(1);
-            code.Value.Add(2);
+            code.Value.Add(a);
+            code.Value.Add(b);
             code.Value.Add(_reg.Add(EOperation.Plus));
 
             _exec.Continue(coro);
 
             var data = _exec.DataStack;
             Assert.AreEqual(1, data.Count);
-            Assert.AreEqual(3, data.Pop());
+            Assert.AreEqual(sum, data.Pop());
+        }
+
+        [Test]
+        public void TestIntegers()
+        {
+            TestPrimitives(1,2,3);
+        }
+
+        [Test]
+        public void TestStrings()
+        {
+            TestPrimitives("foo", "bar", "barfoo");
         }
     }
 }
