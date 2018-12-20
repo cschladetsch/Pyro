1c905dd803aa58c4043b9b725c016cc67c821612
Christian Schladetsch <christian.schladetsch@gmail.com>
2018-12-20 17:45:25 +1100

    Starting Executor

--- Library/Executor/Continuation.cs
+++ Library/Executor/Continuation.cs
@@ -1,16 +1,48 @@
-﻿using System.Collections.Generic;
-using System.Reflection.Emit;
+﻿using System;
+using System.Collections.Generic;
 
 namespace Diver.Executor
 {
     public class Continuation
     {
+        public Continuation()
+        {
+        }
+
         public Continuation(IRef<List<object>> code)
         {
             _code = code.Value;
         }
 
+        public bool HasScopeObject(string label)
+        {
+            return _scope.ContainsValue(label);
+        }
+
+        public void SetScopeObject(string label, object val)
+        {
+            _scope[label] = val;
+        }
+
+        public object FromScope(string label)
+        {
+            return _scope.TryGetValue(label, out var value) ? value : null;
+        }
+
+        public bool Next(out object next)
+        {
+            var has = _next < _scope.Count;
+            next = has ? _code[_next++] : null;
+            return has;
+        }
+
+        public void Reset()
+        {
+            _next = 0;
+        }
+
+        private int _next;
         private List<object> _code = new List<object>();
-        private Dictionary<Label, object> _scope = new Dictionary<Label, object>();
+        private Dictionary<string, object> _scope = new Dictionary<string, object>();
     }
 }
--- Library/Executor/Executor.cs
+++ Library/Executor/Executor.cs
@@ -2,6 +2,35 @@
 
 namespace Diver.Executor
 {
+    public enum EOperation
+    {
+        Plus,
+        Minus,
+        Multiply,
+        Divide,
+
+        Store,
+        Retrieve,
+
+        Suspend,
+        Resume,
+        Replace,
+        
+        Print,
+        Assert,
+
+        If,
+        IfThen,
+        IfThenElse,
+
+        ToArray,
+        ToMap,
+        ToSet,
+        ToPair,
+
+        Expand,
+    }
+
     public class Executor
     {
         public List<object> Data => _data;
@@ -9,7 +38,15 @@ namespace Diver.Executor
 
         void Continue(Continuation cont)
         {
+            object next;
+            while (cont.Next(out next))
+            {
+                if (next is IRefBase obj)
+                {
+                }
+            }
         }
+
 
         private List<object> _data = new List<object>();
         private List<object> _context = new List<object>();
