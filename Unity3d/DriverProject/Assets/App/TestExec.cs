69690867d0535593d257c3edc9c77ed36301b830
Christian Schladetsch <christian.schladetsch@gmail.com>
2018-12-22 04:44:03 +1100

    Test executor from a Unity3d app

---
+++ Unity3d/DriverProject/Assets/App/TestExec.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Diver;
+using Diver.Exec;
+using Diver.Impl;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+public class TestExec : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        IRegistry _reg = new Registry();
+        var code = _reg.Add(new List<object>());
+        var coro = _reg.Add(new Continuation(code.Value));
+
+        code.Value.Add(1);
+        code.Value.Add(2);
+        code.Value.Add(_reg.Add(EOperation.Plus));
+
+        var executor = _reg.Add(new Executor());
+        var exec = executor.Value;
+        exec.Continue(coro);
+
+        var data = exec.DataStack;
+        Assert.AreEqual(1, data.Count);
+        Assert.AreEqual(3, data.Pop());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
