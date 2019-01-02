1a3666a99cd0ff7e7313278ddc094992d04ff843
Christian Schladetsch <christian.schladetsch@gmail.com>
2019-01-03 02:35:45 +1100

    Add Pi interactive console to VR app

--- Unity3d/DiverProject/Assets/App/PiConsole.cs
+++ Unity3d/DiverProject/Assets/App/PiConsole.cs
@@ -1,4 +1,9 @@
-﻿using UIWidgets;
+﻿using System.Text;
+using Diver;
+using Diver.Exec;
+using Diver.Impl;
+using Diver.Language;
+using UIWidgets;
 using UnityEngine;
 
 namespace App
@@ -7,24 +12,51 @@ namespace App
     {
         public TreeView TreeView;
         protected internal ObservableList<ListNode<TreeViewItem>> _treeViewDataSource;
+        private PiTranslator _pi;
+        private IRegistry _registry;
+        private Executor _exec;
 
         void Awake()
         {
+            _registry = new Registry();
+            _pi = new PiTranslator(_registry);
+            _exec = new Executor();
+
             TreeView.Init();
             _treeViewDataSource = new ObservableList<ListNode<TreeViewItem>>();
             TreeView.DataSource = _treeViewDataSource;
 
+            //_pi.Translate("1 2 +");
+            //_exec.Continue(_pi.Continuation);
+            //Debug.Log(_exec.DataStack.Peek());
         }
+
         public void Process(string text)
         {
             Debug.Log(text);
-            var treeViewItem = new TreeViewItem(text);
+            //AddOutputItem(text);
+            //return;
+
+            if (_pi.Translate(text))
+            {
+                _exec.Continue(_pi.Continuation);
+                var index = 0;
+                foreach (var obj in _exec.DataStack)
+                {
+                    AddOutputItem($"{index++}: {obj}");
+                }
+
+                return;
+            }
+
+            AddOutputItem(_pi.Error);
+        }
+
+        private void AddOutputItem(string output)
+        {
+            var treeViewItem = new TreeViewItem(output);
             var treeNode = new TreeNode<TreeViewItem>(treeViewItem);
             _treeViewDataSource.Add(new ListNode<TreeViewItem>(treeNode, 0));
-
-            //var item = new TreeNode<TreeViewItem>();
-            //var node = new ListNode<TreeViewItem>(item, 0);
-            //data.Add(node);
         }
     }
 }
