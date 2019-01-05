269a09389962753680921579793bed3344666863
Christian Schladetsch <christian.schladetsch@gmail.com>
2019-01-05 16:11:52 +1100

    Remove old UWP apps

--- Test/TestRho/TestRhoLoops.cs
+++ Test/TestRho/TestRhoLoops.cs
@@ -17,6 +17,17 @@ assert(c == 1 + 2 + 3)
 ", true);
             
         }
-        
+
+        [Test]
+        public void TestForEachLoop()
+        {
+            RhoRun(@"
+c = 0
+for (a = 0; a < 8; ++a)
+	writeln(a)
+	c = c + a
+assert(c == 1 + 2 + 3 + 4 + 5 + 6 + 7)
+", true);
+        }
     }
 }
