3b1ab0c28e4d11158915dc63cf7ef031abca94c6
Christian Schladetsch <christian.schladetsch@gmail.com>
2019-01-16 00:05:32 +1100

    Add paths

---
+++ Library/Core/ITextSerialise.cs
@@ -0,0 +1,27 @@
+﻿namespace Diver
+{
+    /// <summary>
+    /// Can be serialised to/from strings.
+    ///
+    /// Default is to use the object's ToString() method, and throw on FromText()
+    /// </summary>
+    public interface ITextSerialise
+    {
+        /// <summary>
+        /// Use ToString by default
+        /// </summary>
+        /// <param name="reg"></param>
+        /// <returns></returns>
+        string ToText(IRegistry reg = null);
+
+        /// <summary>
+        /// Throws NotImplemented by default;
+        /// </summary>
+        /// <returns>true if the conversion succeeded</returns>
+        bool FromText(string s, IRegistry reg);
+
+        bool FromText(StringSlice s, IRegistry reg);
+    }
+
+    // like a Language.Slice, but not based on a Lexer
+}
