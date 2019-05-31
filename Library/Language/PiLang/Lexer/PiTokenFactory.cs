50a2978312342a731bc9200000b339305ae4513a
Christian <christian.schladetsch@gmail.com>
2019-06-01 07:31:18 +1000

    Formatting

--- Library/Language/PiLang/Lexer/PiTokenFactory.cs
+++ Library/Language/PiLang/Lexer/PiTokenFactory.cs
@@ -10,29 +10,11 @@
     {
         private LexerBase _lexer;
 
-        public PiToken NewToken(EPiToken en, Slice slice)
-        {
-            return new PiToken(en, slice);
-        }
-
-        public PiToken NewTokenIdent(Slice slice)
-        {
-            return NewToken(EPiToken.Ident, slice);
-        }
-
-        public PiToken NewTokenString(Slice slice)
-        {
-            return NewToken(EPiToken.String, slice);
-        }
-
-        public PiToken NewEmptyToken(Slice slice)
-        {
-            return NewToken(EPiToken.None, slice);
-        }
-
-        public void SetLexer(LexerBase lexer)
-        {
-            _lexer = lexer;
-        }
+        public void SetLexer(LexerBase lexer) => _lexer = lexer;
+        public PiToken NewToken(EPiToken en, Slice slice) => new PiToken(en, slice);
+        public PiToken NewTokenIdent(Slice slice) => NewToken(EPiToken.Ident, slice);
+        public PiToken NewTokenString(Slice slice) => NewToken(EPiToken.String, slice);
+        public PiToken NewEmptyToken(Slice slice) => NewToken(EPiToken.None, slice);
     }
 }
+
