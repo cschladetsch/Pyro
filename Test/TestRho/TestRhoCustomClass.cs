f15d03d3c44d289bcd8996ef8d971c34a2e9bad8
Christian <christian.schladetsch@gmail.com>
2019-05-31 19:53:44 +1000

    Test 'new' operator

--- Test/TestRho/TestRhoCustomClass.cs
+++ Test/TestRho/TestRhoCustomClass.cs
@@ -1,11 +1,12 @@
-﻿using System;
-using NUnit.Framework;
+﻿using NUnit.Framework;
+
 using Pyro;
 
 namespace Diver.Test.Rho
 {
     [TestFixture]
-    public class TestRhoCustomClass : TestCommon
+    public class TestRhoCustomClass
+        : TestCommon
     {
         public class Foo
         {
@@ -28,13 +29,13 @@ namespace Diver.Test.Rho
         [Test]
         public void TestCustomClass()
         {
+            // TODO: use reflection to find methods if not explicitly provided via ClassBuilder<> object
             _reg.Register(new ClassBuilder<Foo>(_reg)
                 .Methods
                     .Add<int,int,int>("Sum", (q,a,b) => q.Sum(a,b))
                     .Add<string,int,string>("MulString", (q,a,b) => q.MulString(a,b))
                 .Class);
 
-            var start = DateTime.Now;
             RhoRun(
 @"
 fun foo(type)
@@ -44,16 +45,6 @@ fun foo(type)
 	assert(a.MulString(""foo"", 3) + ""bar"" == ""foofoofoobar"")
 foo(Foo)
 ");
-            var duration = DateTime.Now - start;
-            WriteLine($"TotalTime {duration.TotalMilliseconds}ms");
-            _continuation.Reset();
-
-            start = DateTime.Now;
-            _continuation.Reset();
-	        _exec.Clear();
-            _exec.Continue(_continuation);
-	        var numOps = _exec.NumOps;
-            WriteLine($"Just execution {numOps} ops: {(DateTime.Now - start).TotalMilliseconds}ms");
         }
     }
 }
