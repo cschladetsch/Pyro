e61ea81d2832045bafa6280a67deeef00a017e31
Christian <christian.schladetsch@gmail.com>
2019-06-08 03:10:03 +1000

    Move Continuation.OpToText to a new partial class file

--- Library/Executor/CannotEnumerate.cs
+++ Library/Executor/CannotEnumerate.cs
@@ -2,21 +2,16 @@
 
 namespace Pyro.Exec
 {
-    [Serializable]
     internal class CannotEnumerate
         : Exception
     {
         private readonly object _obj;
 
         public CannotEnumerate(object obj)
-        {
-            _obj = obj;
-        }
+            => _obj = obj;
 
         public override string ToString()
-        {
-            return $"Cannot enumerate over {_obj.GetType().Name}";
-        }
+            => $"Cannot enumerate over {_obj.GetType().Name}";
     }
 }
 
--- Library/Executor/Continuation.cs
+++ Library/Executor/Continuation.cs
@@ -3,9 +3,9 @@ using System.Collections.Generic;
 
 namespace Pyro.Exec
 {
+    /// <inheritdoc />
     /// <summary>
     /// Also known as a co-routine.
-    ///
     /// Can be interrupted mid-execution and later resumed.
     /// </summary>
     public partial class Continuation
@@ -20,9 +20,7 @@ namespace Pyro.Exec
         private IDictionary<string, object> _scope => Scope;
 
         public Continuation(IList<object> code)
-        {
-            Code = code;
-        }
+            => Code = code;
 
         /// <summary>
         /// Helper to make a new continuation, which also uses a referenced list for scope
@@ -57,171 +55,6 @@ namespace Pyro.Exec
             }
 
             str.Append('}');
-        }
-
-        private static string OpToText(EOperation op)
-        {
-            switch (op)
-            {
-            case EOperation.Nop:
-                return "nop";
-            case EOperation.HasType:
-                break;
-            case EOperation.GarbageCollect:
-                return "garbage_collect";
-            case EOperation.Plus:
-                return "+";
-            case EOperation.Minus:
-                return "-";
-            case EOperation.Multiply:
-                return "*";
-            case EOperation.Divide:
-                return "/";
-            case EOperation.Modulo:
-                return "%";
-            case EOperation.Store:
-                return "#";
-            case EOperation.Has:
-                return "has";
-            case EOperation.Retrieve:
-                return "@";
-            case EOperation.Assign:
-                break;
-            case EOperation.GetPath:
-                break;
-            case EOperation.Suspend:
-                return "&";
-            case EOperation.Resume:
-                return "...";
-            case EOperation.Replace:
-                return "!";
-            case EOperation.Assert:
-                return "assert";
-            case EOperation.Write:
-                return "write";
-            case EOperation.WriteLine:
-                return "writeln";
-            case EOperation.If:
-                return "if";
-            case EOperation.IfElse:
-                return "ife";
-            case EOperation.StackToList:
-                break;
-            case EOperation.ListToStack:
-                break;
-            case EOperation.Depth:
-                break;
-            case EOperation.Dup:
-                return "dup";
-            case EOperation.Clear:
-                return "clear";
-            case EOperation.Swap:
-                return "swap";
-            case EOperation.Break:
-                return "break";
-            case EOperation.Rot:
-                break;
-            case EOperation.Roll:
-                return "roll";
-            case EOperation.RotN:
-                return "rotn";
-            case EOperation.RollN:
-                return "rolln";
-            case EOperation.Pick:
-                return "pick";
-            case EOperation.Over:
-                return "over";
-            case EOperation.Freeze:
-                return "freeze";
-            case EOperation.Thaw:
-                return "thaw";
-            case EOperation.FreezeText:
-                return "freezet";
-            case EOperation.ThawText:
-                return "thawt";
-            case EOperation.FreezeYaml:
-                return "freezey";
-            case EOperation.ThawYaml:
-                return "thawy";
-            case EOperation.Not:
-                return "!";
-            case EOperation.Equiv:
-                return "==";
-            case EOperation.LogicalAnd:
-                return "&&";
-            case EOperation.LogicalOr:
-                return "||";
-            case EOperation.LogicalXor:
-                return "^";
-            case EOperation.Less:
-                return "<";
-            case EOperation.Greater:
-                return ">";
-            case EOperation.GreaterOrEquiv:
-                return ">=";
-            case EOperation.LessOrEquiv:
-                return "<=";
-            case EOperation.NotEquiv:
-                return "!=";
-            case EOperation.Expand:
-                return "expand";
-            case EOperation.ToArray:
-                return "to_array";
-            case EOperation.ToMap:
-                return "to_map";
-            case EOperation.ToSet:
-                return "to_set";
-            case EOperation.ToPair:
-                return "to_pair";
-            case EOperation.Size:
-                return "size";
-            case EOperation.GetBack:
-                return "back";
-            case EOperation.PushBack:
-                return "push_back";
-            case EOperation.PushFront:
-                return "push_front";
-            case EOperation.ToList:
-                return "tolist";
-            case EOperation.Remove:
-                return "remove";
-            case EOperation.Insert:
-                return "insert";
-            case EOperation.New:
-                return "new";
-            case EOperation.At:
-                return "@";
-            case EOperation.DebugPrintDataStack:
-                return "debug_print_data";
-            case EOperation.DebugPrintContextStack:
-                return "debug_print_context";
-            case EOperation.DebugPrint:
-                return "debug_print";
-            case EOperation.DebugPrintContinuation:
-                return "debug_print_cont";
-            case EOperation.DebugSetLevel:
-                return "set_debug_level";
-            case EOperation.SetFloatPrecision:
-                return "set_float_precision";
-            case EOperation.Self:
-                return "self";
-            case EOperation.GetMember:
-                return "get_member";
-            case EOperation.SetMember:
-                return "set_member";
-            case EOperation.SetMemberValue:
-                return "set_member_value";
-            case EOperation.ForEachIn:
-                return "for_each";
-            case EOperation.ForLoop:
-                return "for";
-            case EOperation.Drop:
-                return "drop";
-            case EOperation.DropN:
-                return "dropn";
-            }
-
-            return $"`{(int) op}";
         }
 
         public static void Register(IRegistry reg)
