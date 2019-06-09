0781d4287bf1d40a5338df654c27adac6b314961
Christian <christian.schladetsch@gmail.com>
2019-06-09 20:43:19 +1000

    Formatting

--- Library/Core/Impl/VoidCallable.cs
+++ Library/Core/Impl/VoidCallable.cs
@@ -3,12 +3,17 @@ using System.Collections.Generic;
 
 namespace Pyro.Impl
 {
-    public class VoidCallable<T, A, B> : CallableBase, ICallable<T> where T : class
+    /// <summary>
+    /// A callable thing that doesn't return anything and takes two arguments.
+    /// </summary>
+    public class VoidCallable<T, A, B>
+        : CallableBase, ICallable<T>
+            where T : class
     {
-        public VoidCallable(Action<T, A, B> fun) : base(fun)
-        {
-            _fun = fun;
-        }
+        private readonly Action<T, A, B> _fun;
+
+        public VoidCallable(Action<T, A, B> fun)
+            : base(fun) => _fun = fun;
 
         public override void Invoke(IRegistry reg, Stack<object> stack)
         {
@@ -17,16 +22,16 @@ namespace Pyro.Impl
             var b = stack.Pop();
             _fun(reg.Get<T>(obj), reg.Get<A>(a), reg.Get<B>(b));
         }
-
-        private readonly Action<T, A, B> _fun;
     }
 
-    public class VoidCallable<T, A> : CallableBase, ICallable<T> where T : class
+    public class VoidCallable<T, A>
+        : CallableBase, ICallable<T>
+            where T : class
     {
-        public VoidCallable(Action<T, A> fun) : base(fun)
-        {
-            _fun = fun;
-        }
+        private readonly Action<T, A> _fun;
+
+        public VoidCallable(Action<T, A> fun)
+            : base(fun) => _fun = fun;
 
         public override void Invoke(IRegistry reg, Stack<object> stack)
         {
@@ -34,23 +39,21 @@ namespace Pyro.Impl
             var a = stack.Pop();
             _fun(reg.Get<T>(obj), reg.Get<A>(a));
         }
-
-        private readonly Action<T, A> _fun;
     }
 
-    public class VoidCallable<T> : CallableBase, ICallable<T> where T : class
+    public class VoidCallable<T>
+        : CallableBase, ICallable<T>
+            where T : class
     {
-        public VoidCallable(Action<T> fun) : base(fun)
-        {
-            _fun = fun;
-        }
+        private readonly Action<T> _fun;
+
+        public VoidCallable(Action<T> fun)
+            : base(fun) => _fun = fun;
 
         public override void Invoke(IRegistry reg, Stack<object> stack)
         {
             var obj = stack.Pop();
             _fun(reg.Get<T>(obj));
         }
-
-        private readonly Action<T> _fun;
     }
 }
