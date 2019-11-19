ca7d7fc563c5ccd15dd577727129c1be7473002e
Christian <christian@realresponse.com.au>
2019-11-19 12:53:53 +1100

    Add dummy methods to console client.

--- App/Console/Program.cs
+++ App/Console/Program.cs
@@ -69,8 +69,14 @@
             }
         }
 
+        /// <summary>
+        /// Invoked server-side when a new client connects
+        /// </summary>
+        /// <param name="peer"></param>
+        /// <param name="client"></param>
         private void OnConnected(IPeer peer, IClient client)
         {
+            //WriteLine("OnConnected: adding methods");
             var scope = client.Context.Executor.Scope;
             scope["Connected"] = Pyro.Create.Method<TestClient, string, int>((Q,name,id) => Q.Connected(name, id));
             scope["UpdateTransform"] = Pyro.Create.Method<TestClient, int,float,float,float>((Q,id,x,y,z) => Q.UpdateTransform(id,x,y,z));
