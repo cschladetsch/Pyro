8b4255d2488bb8f4570a106c04e869468f3d68b4
Christian Schladetsch <c.schladetsch@tektindustries.com>
2023-03-08 09:06:03 +1100

    Starting on Pathnames for Tree

--- Library/Core/IPathname.cs
+++ Library/Core/IPathname.cs
@@ -1,9 +1,36 @@
 ﻿namespace Pyro {
+
+    using System.Collections.Generic;
+
+    public class PathElement {
+        public enum EType {
+            None,
+            Separator,
+            Identifier,
+        }
+
+        public EType ElementType { get; set; }
+        public IIdentifer Identifer { get; }
+
+        public PathElement(IIdentifer identifer) {
+            ElementType = EType.Identifier;
+            Identifer = identifer;
+        }
+
+        public PathElement(EType type) {
+            ElementType = type;
+        }
+    }
+
     /// <inheritdoc />
     /// <summary>
     /// DOC
     /// </summary>
     public interface IPathname
         : IIdentifer {
+
+        bool IsAbsolute { get; }
+
+        List<PathElement> Elements { get; }
     }
 }
--- Library/Core/Impl/Tree.cs
+++ Library/Core/Impl/Tree.cs
@@ -9,6 +9,30 @@ namespace Pyro.Impl {
         public IConstRefBase Parent { get; }
         public IList<IConstRefBase> SearchPath { get; set; }
 
+        public IConstRefBase Find(IPathname path) {
+            return null;
+        }
+
+        public IEnumerable<IConstRefBase> GetChildren(IPathname path) {
+            //if (path.IsAbsolute) {
+            //    var node = Root;
+            //    foreach (var element in path.Elements) {
+            //        if (element.ElementType == PathElement.EType.Separator)
+            //            continue;
+
+            //        if (element.ElementType == PathElement.EType.Identifier) {
+            //            if (node is IConstRefBase cr)
+            //                node = cr.Scope[element.Identifer.ToText()] as IConstRefBase;
+            //            else
+            //                node = null;
+            //        }
+            //    }
+
+            //    return node;
+            //}
+            return default;
+        }
+
         public object Resolve(IIdentifer ident) {
             if (!(Scope is IConstRefBase cr))
                 return GetNative(ident.ToText());
